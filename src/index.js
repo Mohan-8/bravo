5d922444e7c25f5ce506b72f00bbeacfd63dc23d
Mohan-8 <smohanakrishnan82@gmail.com>
2023-07-24 20:54:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -11,7 +11,7 @@ root.render(
     <DAppProvider config={{}}>
       <Router>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<App/>} />
         </Routes>
       </Router>
     </DAppProvider>
