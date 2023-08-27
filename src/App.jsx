702d58e576a28f889d79b755367696b36c655142
Mohan-8 <smohanakrishnan82@gmail.com>
2023-08-27 17:45:18 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,24 @@ import Buy from "./components/Buy/Buy";
 import Buyb from "./components/Buy/Buyb";
 import Tokenomics from "./components/Tokenomics/Tokenomics";
 import Footer from "./components/Footer/Footer";
-import React, { useState } from "react";
-import LoadingScreen from "./components/Loading/LoadingScreen";
 
 function App() {
-  let [loading, setLoading] = useState(true);
-  const simulateAsyncTask = () => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  };
-  React.useEffect(() => {
-    simulateAsyncTask();
-  }, []);
   return (
-    <div className="app">
-      {loading ? <LoadingScreen /> : null}
-      <Container>
-        <Banner>
-          <Header />
-          <ProfComponent />
-        </Banner>
-        <Buy />
-        <Buyb />
-        <LightColor>
-          <Members />
-        </LightColor>
-        <Tokenomics />
-        <LightColor>
-          <Footer />
-        </LightColor>
-      </Container>
-    </div>
+    <Container>
+      <Banner>
+        <Header />
+        <ProfComponent />
+      </Banner>
+      <Buy />
+      <Buyb />
+      <LightColor>
+        <Members />
+      </LightColor>
+      <Tokenomics />
+      <LightColor>
+        <Footer />
+      </LightColor>
+    </Container>
   );
 }
 
